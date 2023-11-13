16dc57a13e2ab1cd4e61080d88eca25e2e0a0fde
YanaZuhova <ziukhovaiana@gmail.com>
2023-11-13 15:13:25 +0200

    JS3.24

---
+++ 3-24.js
@@ -0,0 +1,24 @@
+function userBuilder(name, age) {
+  const nameDefault = "username";
+  const ageDefault = 99;
+
+  if (!name) name = nameDefault;
+  if (!age) age = ageDefault;
+
+  function createUser() {
+    return {
+      name: name,
+      age: age,
+    };
+  }
+
+  return createUser;
+}
+
+let userCreator1 = userBuilder("Yana", 25);
+let userCreator2 = userBuilder("Alex");
+let userCreator3 = userBuilder();
+
+console.log(userCreator1);
+console.log(userCreator2);
+console.log(userCreator3);
